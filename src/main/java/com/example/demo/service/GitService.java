650723fa8bf7e4e1c13550837d597eef3011bb2c
tim <timpham2003@gmail.com>
2023-03-28 15:21:10 -0500

    add model validation

--- src/main/java/com/example/demo/service/GitService.java
+++ src/main/java/com/example/demo/service/GitService.java
@@ -2,6 +2,7 @@ package com.example.demo.service;
 
 import com.example.demo.entity.UserInfo;
 import com.example.demo.entity.UserRepo;
+import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -29,7 +30,7 @@ public class GitService {
         this.baseUrl = baseUrl;
     }
 
-    public Mono<UserInfo> getUserInfoAsync(String userId) {
+    public Mono<UserInfo> getUserInfoAsync(@NonNull String userId) {
         log.info("getting userInfo for {}", userId);
         return webClient.get()
                 .uri(baseUrl + "/{userId}", userId)
@@ -37,7 +38,7 @@ public class GitService {
                 .bodyToMono(UserInfo.class);
     }
 
-    public Mono<UserRepo[]> getUserReposAsync(String userId) {
+    public Mono<UserRepo[]> getUserReposAsync(@NonNull String userId) {
         log.info("getting repo for {}", userId);
         return webClient.get()
                 .uri(baseUrl + "/{userId}/repos", userId)
